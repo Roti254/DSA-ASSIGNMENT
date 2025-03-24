6f31ae7d8f09776a9e8c08ad38656c264583cf9d
Cathy <rotichcathy62@gmail.com>
2025-03-24 20:51:49 +0300

    Added LinkedList.java with insertion and deletion methods

--- Linkedlist.java
+++ Linkedlist.java
@@ -1,1 +1,69 @@
+class LInkedlist{
+    private class Node{
+        int data;
+        Node next;
 
+        publicNode(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    private Node head;
+
+    //insert node at the beginning
+    public void insertAtBeginning(int data){
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+    }
+
+    //insert node at end
+    public void insertAtEnd(int data){
+        Node newNode = new Node(data);
+        if(head ==null){
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while(temp.next != null){
+            temp = temp.next;
+        }
+        temp.next = newNode;
+
+    }
+
+    //delete node from beginning
+    public void deleteFromBeginnin(){
+        if(head == null){
+            System.out.println("Empty list.Nothing to delete.");
+            return;
+        }
+        head = head.next;
+    }
+
+    //display linked list
+    public void display(){
+        if(head == null){
+            System.out.println("Empty list.");
+            return;
+        }
+        Node temp = head;
+        while(temp != null){
+            System.out.println(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[]args){
+        LinkedList list = new LinkedList();
+        list.insertAtBeginning(5);
+        list.insertAtBeginning(15);
+        list.insertAtEnd(25);
+        list.display();
+
+        list.deleteFromBeginning();
+        list.display();
+    }
+}
